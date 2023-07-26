f85f97a1e2250c18bdd57e432edbf589a8d6067e
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-07-27 01:27:08 +0600

    full list ts created

---
+++ src/templates/ListTemplate.ts
@@ -0,0 +1,12 @@
+import FullList from "../model/FullList";
+
+interface DOMList {
+    ul: HTMLUListElement,
+    clear(): void,
+    render(fullList: FullList): void,
+
+}
+
+export default class ListTemplate implements DOMList {
+
+}
